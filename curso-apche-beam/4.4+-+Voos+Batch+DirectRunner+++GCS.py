f8316130fda8f881cb74865bffd8f42089290318
Camila Yukari Yatabe <123522455+camiyuka@users.noreply.github.com>
2024-02-29 16:03:17 -0300

    Add files via upload

---
+++ curso-apche-beam/4.4+-+Voos+Batch+DirectRunner+++GCS.py
@@ -0,0 +1,39 @@
+# mandando dados tratados para o bucket da cloud storage como saída 
+import apache_beam as beam
+import os
+
+serviceAccount = r"C:\\Users\\camil\Downloads\dataflow-apache-beam-415812-f900aff91ba8.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = serviceAccount
+
+p1 = beam.Pipeline()
+
+class filtro(beam.DoFn):
+  def process(self,record):
+    if int(record[8]) > 0:
+      return [record]
+
+Tempo_Atrasos = (
+  p1
+  | "Importar Dados Atraso" >> beam.io.ReadFromText(r"C:\\Users\\camil\Downloads\\voos_sample.csv", skip_header_lines = 1)
+  | "Separar por Vírgulas Atraso" >> beam.Map(lambda record: record.split(','))
+  | "Pegar voos com atraso" >> beam.ParDo(filtro())
+  | "Criar par atraso" >> beam.Map(lambda record: (record[4],int(record[8])))
+  | "Somar por key" >> beam.CombinePerKey(sum)
+)
+
+Qtd_Atrasos = (
+  p1
+  | "Importar Dados" >> beam.io.ReadFromText(r"C:\\Users\\camil\Downloads\\voos_sample.csv", skip_header_lines = 1)
+  | "Separar por Vírgulas Qtd" >> beam.Map(lambda record: record.split(','))
+  | "Pegar voos com Qtd" >> beam.ParDo(filtro())
+  | "Criar par Qtd" >> beam.Map(lambda record: (record[4],int(record[8])))
+  | "Contar por key" >> beam.combiners.Count.PerKey()
+)
+
+tabela_atrasos = (
+    {'Qtd_Atrasos':Qtd_Atrasos,'Tempo_Atrasos':Tempo_Atrasos} 
+    | "Group By" >> beam.CoGroupByKey()
+    | "Saida Para GCP" >> beam.io.WriteToText(r"gs://apache_dataflow-udemy/entrada/voos_sample.csv")
+)
+
+p1.run()
